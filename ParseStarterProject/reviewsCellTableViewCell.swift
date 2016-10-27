a49270b5efb59855053b272f3f2fe245f487e003
eliteman22 <flaviolici@gmail.com>
2016-10-27 13:49:17 -0400

    first commit

---
+++ ParseStarterProject/reviewsCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  reviewsCellTableViewCell.swift
+//  Questly
+//
+//  Created by Flavio Lici on 11/12/15.
+//  Copyright (c) 2015 Parse. All rights reserved.
+//
+
+import UIKit
+
+class reviewsCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
